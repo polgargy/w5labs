a2c14dcabd7e7e174e60118c44fe56138876337c
Polgargy <polgargy91@gmail.com>
2020-11-23 14:13:10 +0100

    mod: cleanup

--- api/routes/web.php
+++ api/routes/web.php
@@ -12,11 +12,3 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
-})->name('dashboard');
